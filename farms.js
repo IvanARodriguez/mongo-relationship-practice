d3e46247e114c570ff1a83e5f218497c535751c4
Ivan A Rodriguez <ivancalvo91@gmail.com>
2025-09-10 21:46:13 -0400

    Adding error handling for better practice

--- farms.js
+++ farms.js
@@ -8,23 +8,29 @@ database.on('error', console.error.bind(console, 'database connection error'));
 
 database.once('open', () => console.log('Database connected'));
 
+const productSchema = new mongoose.Schema({
+	name: String,
+	price: Number,
+	season: {
+		type: String,
+		enum: ['Spring', 'Summer', 'Fall', 'Winter'],
+	},
+});
+
+const Product = mongoose.model('Product', productSchema);
+
 async function seedProduct() {
-	const productSchema = new mongoose.Schema({
-		name: String,
-		price: Number,
-		season: {
-			type: String,
-			enum: ['Spring', 'Summer', 'Fall', 'Winter'],
-		},
-	});
-
-	const Product = mongoose.model('Product', productSchema);
-
-	await Product.insertMany([
-		{ name: 'Goddess Melon', price: 4.99, season: 'Summer' },
-		{ name: 'Sugar Baby Watermelon', price: 4.99, season: 'Winter' },
-		{ name: 'Asparagus', price: 2.99, season: 'Spring' },
-	]);
+	try {
+		await Product.deleteMany({});
+		await Product.insertMany([
+			{ name: 'Goddess Melon', price: 4.99, season: 'Summer' },
+			{ name: 'Sugar Baby Watermelon', price: 4.99, season: 'Winter' },
+			{ name: 'Asparagus', price: 2.99, season: 'Spring' },
+		]);
+		console.log('Products seeded!');
+	} catch (error) {
+		console.error('Seeding error:', err);
+	}
 }
 
 seedProduct().then(() => {
